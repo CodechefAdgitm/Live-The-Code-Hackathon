2a1b55198636c36189080a1b6acb20f81dbede55
Nitin <nitinrana01125532553@gmail.com>
2023-07-30 21:34:20 +0530

    Added typewriter effect

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./hero.css"
 import image from "../../assets/images/22635333_6583017.svg";   
+import Typewriter from 'typewriter-effect'
 
 export default function Hero() {
   return (
@@ -8,7 +9,16 @@ export default function Hero() {
       <div className="hero-section">
         <div className="left">
           <h1>Welcome To Our <span>Hackathon</span></h1>
-          <p>a weekend of innovation and creativity awaits! Join us for electrifying challenges, collaborative brilliance, and the chance to shape the future. Unleash y            our potential and be part of a vibrant community driving positive change. Let's hack together!</p>
+          <h2>
+            <Typewriter 
+              options={{
+                strings: ['A weekend of innovation', 'A weekend of creativity', 'A weekend of challenges'],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </h2>
+          <p>A weekend of innovation and creativity awaits! Join us for electrifying challenges, collaborative brilliance, and the chance to shape the future. Unleash y            our potential and be part of a vibrant community driving positive change. Let's hack together!</p>
           <div className="action-buttons">
             <button className="apply-button">Apply Now</button>
           </div>
