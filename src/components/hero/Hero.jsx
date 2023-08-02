e0ca10f9c9755f238b67dd2f7229f708e5b8f86c
Nitin <nitinrana01125532553@gmail.com>
2023-08-03 00:38:31 +0530

    integrated apply with devfolio button

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,15 +4,15 @@ import image from "../../assets/images/22635333_6583017.svg";
 
 export default function Hero() {
   useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://apply.devfolio.co/v2/sdk.js";
+    const script = document.createElement('script');
+    script.src = 'https://apply.devfolio.co/v2/sdk.js';
     script.async = true;
     script.defer = true;
     document.body.appendChild(script);
     return () => {
       document.body.removeChild(script);
-    };
-  }, []);
+    }
+}, []);
   return (
     <>
       <div className="hero-section">
@@ -27,14 +27,7 @@ export default function Hero() {
             shape the future. Unleash y our potential and be part of a vibrant
             community driving positive change. Let's hack together!
           </p>
-          <div className="action-buttons">
-            {/* <button className="apply-button" >Apply With Devfolio</button> */}
-            {/* <div
-              className="apply-button"
-              data-hackathon-slug="Live The Code"
-              data-button-theme="dark"
-              style={{ height: "44px", width: "312px" }}
-            ></div> */}
+          {/* <div className="action-buttons">
             <a
               className="apply-button"
               target="_blank"
@@ -42,6 +35,15 @@ export default function Hero() {
             >
               Apply With Devfolio
             </a>
+          </div> */}
+          <div className="action-buttons">
+            <div 
+              className="apply-button"
+              data-hackathon-slug="Live-The-Code-Hackathon"
+              data-button-theme="dark"
+              style={{ height: '44px', width: '312px' }}
+            >
+            </div>
           </div>
         </div>
         <div className="right">
