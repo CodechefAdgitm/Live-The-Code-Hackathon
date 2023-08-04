88871c1a710e91c77687d33c0e75c109530e25d0
= <=>
2023-08-04 20:57:15 +0530

    apply btn and navbar logo

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,15 +4,15 @@ import image from "../../assets/images/22635333_6583017.svg";
 
 export default function Hero() {
   useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://apply.devfolio.co/v2/sdk.js';
+    const script = document.createElement("script");
+    script.src = "https://apply.devfolio.co/v2/sdk.js";
     script.async = true;
     script.defer = true;
     document.body.appendChild(script);
     return () => {
       document.body.removeChild(script);
-    }
-}, []);
+    };
+  }, []);
   return (
     <>
       <div className="hero-section">
@@ -25,25 +25,26 @@ export default function Hero() {
             a weekend of innovation and creativity awaits! Join us for
             electrifying challenges, collaborative brilliance, and the chance to
             shape the future. Unleash y our potential and be part of a vibrant
-            community driving positive change. Let's hack together!
+            community driving positive change. Let&apos;s hack together!
           </p>
-          {/* <div className="action-buttons">
+          <div className="temp-button">
             <a
-              className="apply-button"
+              // className="apply-button"
               target="_blank"
+              rel="noreferrer"
               href="https://devfolio.co/@codechefadgitm"
+              style={{color:"white",textDecoration:"None"}}
             >
               Apply With Devfolio
             </a>
-          </div> */}
+          </div>
           <div className="action-buttons">
-            <div 
+            <div
               className="apply-button"
               data-hackathon-slug="Live-The-Code-Hackathon"
               data-button-theme="dark"
-              style={{ height: '44px', width: '312px' }}
-            >
-            </div>
+              style={{ height: "44px", width: "312px" }}
+            ></div>
           </div>
         </div>
         <div className="right">
