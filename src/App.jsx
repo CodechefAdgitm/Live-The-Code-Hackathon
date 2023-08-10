c6f01768f288207cd7a988ac4936cf4275e2e745
AnishHazra <anishhazra108@gmail.com>
2023-08-10 15:37:08 +0530

    remove team and our offerings

--- src/App.jsx
+++ src/App.jsx
@@ -30,8 +30,8 @@ function App() {
         <Rules/>
         <Prize/>
         <Sponsors/>
-        <Teams/>
-        <Offerings/>
+        {/* <Teams/> */}
+        {/* <Offerings/> */}
         <Ask/>
         <Contact/>
       
